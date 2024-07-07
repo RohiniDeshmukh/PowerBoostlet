881d75b87442ba34ece490a28d0bc07aefca3377
Rohini Deshmukh <r.deshmukh001@umb.edu>
2024-07-07 19:04:23 -0400

    fixed run()

--- examples/floatingUI/floatingUI.js
+++ examples/floatingUI/floatingUI.js
@@ -1,17 +1,15 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.src = "https://raw.githubusercontent.com/RohiniDeshmukh/PowerBoostlet/main/dist/powerboost.min.js";
-console.log("is working on my system ")
-script.onload = run;
+console.log("it is working on my system ")
 document.head.appendChild(script);
+script.onload = run();
 eval(script);
 
 function run() {
 
     powerboost.load_links();
-
-
-
+    console.log("inside run...")
     powerboost.load_html(() => { powerboost.load_scripts(() => { powerboost.load_aceEditor(); }); });
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Powerboost } from "./powerboost.js";
 
 // register global namespace with a new powerboost instance
-window.powerboost = new Powerboost();
-window.console.log('powerboost VERSION 0.1-alpha');
+window.Powerboost = new Powerboost();
+window.console.log('Powerboost VERSION 0.1-alpha');
